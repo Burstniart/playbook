774d236cdd8d6b9318b3e3d66048ea8512d61dd4
Alejandro Garcia <80152145+Burstniart@users.noreply.github.com>
2022-04-07 22:05:51 -0500

    Update pokemon.js

--- weekly_mission_1/example10/pokemon.js
+++ weekly_mission_1/example10/pokemon.js
@@ -1,14 +1,3 @@
-// Crea un archivo pokemon.js, ahí crea la clase que necesitas para correr el archivo main.js. 
-// Deberás exportarla usando ESM.
-// Necesitarás usar export default.
-// La clase tiene la misma implementación que el ejercicio anterior.
-// Finalmente corre: node main.js
-
-/*
-  export default nos permite exportar esta clase e importara
-
-  import MyLogger from './logger.js'
-*/
 
 export default class Logger {
     // 
