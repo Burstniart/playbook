03a371789d1999d8f6db376347a80285afb66de3
Alejandro Garcia <v4gr33n@hotmail.com>
2022-04-16 23:41:04 -0500

    Testing -S to sign commits

--- weekly_mission_2/examples/examples5/pokemon.test.js
+++ weekly_mission_2/examples/examples5/pokemon.test.js
@@ -1,6 +1,6 @@
 import Pokemon from './pokemon.js'
 
 test('1) Create a new object pokemon', () => {
-  const myPokemon = new Pokemon('Charmander', "Fuego", 10)
+  const myPokemon = new Pokemon('Charmander', "Fuego", 9)
   expect(myPokemon.name).toBe('Charmander'); // Corrige esta prueba
 });
