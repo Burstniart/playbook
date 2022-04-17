cf2ec05a64f093b9dec693b24cd65f8d92c2d59a
Alejandro Garcia <v4gr33n@hotmail.com>
2022-04-16 20:44:08 -0500

    Tested object instance and static function

--- weekly_mission_2/examples/classAndObj.js
+++ weekly_mission_2/examples/classAndObj.js
@@ -137,8 +137,8 @@ console.log("Ejemplo 8: Métodos static")
 // Puedo llamar el método static directamente con el nombre de la clase
 console.log(Toolbox.getMostUsefulTools())
 // Si intentamos instanciar un objeto, no podremos llamar este método static
-//const toolbox = new Toolbox()
-//console.log(toolbox.getMostUsefulTools()) // is not a function
+// const toolbox = new Toolbox()
+// console.log(toolbox.getMostUsefulTools()) // is not a function
 
 /*
 
