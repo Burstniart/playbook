db9c0075ec5e746906e13d3bd0c26ca21c7b6b16
Burstniart <v4gr33n@hotmail.com>
2022-04-19 16:54:01 -0500

    Add test for our class constructor

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,6 +1,11 @@
-describe("This is a test suit", () => {
-    test('Case test 1', () => {
-        const result = 1 + 2
-        expect(result).toBe(10);
+const MissionCommander = require('./../app/missionCommander');
+
+describe("This is a test Unit Test for Mission Commander", () => {
+    test(' 1) Create a mission commander object', () => {
+        // I test
+        const myMissionCommander = new MissionCommander("Woopa")
+
+        // I expect this
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
