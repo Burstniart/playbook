9084ada727858902a68da8d565a2e1278e19bc3c
Burstniart <v4gr33n@hotmail.com>
2022-04-19 17:25:44 -0500

    Define a test as guide line for a spiderman constructor

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe('Test suit desc', () => {
-    test('Case 1', () => {
-        const res = 1+2
-        expect(res).toBe(18);
+describe('Unit Test for Spiderman CLass', () => {
+    test('1) Create a spiderman object', () => {
+        // I write code here
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, 'Andrew Garfield', 2, 'Sony')
+
+        // I write what the outcome should be
+        expect(andrewGarfield.name).toBe("Spiderman Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.studio).toBe("Sony");
+        
     });
 })
