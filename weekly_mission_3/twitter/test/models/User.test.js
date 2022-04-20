ede7bac393e12637f6dcccabb43c431194a87e4f
Burstniart <v4gr33n@hotmail.com>
2022-04-19 22:31:35 -0500

    Add full test

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -3,9 +3,14 @@
 
 describe('Unit Test for Twitter', () => {
     test('1) Model for User', () => {
-        const myUser = new User('Monika')
+        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!", 'dateCreated', 'lastUpdate')
 
+        expect(myUser.id).toBe(1);
+        expect(myUser.username).toBe('Princess Peach');
         expect(myUser.name).toBe('Monika');
+        expect(myUser.bio).toBe("She's a queen, beach!");
+        expect(myUser.dateCreated).toBe('dateCreated');
+        expect(myUser.lastUpdate).toBe('lastUpdate');
     });
 })
 
