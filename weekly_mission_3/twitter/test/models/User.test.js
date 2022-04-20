590bccd3618814e24b5b27ca06358c2a4e1e4317
Burstniart <v4gr33n@hotmail.com>
2022-04-19 22:40:41 -0500

    Add contructor for class User, pass test
    Signed-off-by: Burstniart <v4gr33n@hotmail.com>

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,16 +1,14 @@
-// Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app: 
-// `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-
+const User = require('./../../app/models/User')
 describe('Unit Test for Twitter', () => {
     test('1) Model for User', () => {
-        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!", 'dateCreated', 'lastUpdate')
+        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!", 'dateCreated', 'lastUpdated')
 
         expect(myUser.id).toBe(1);
         expect(myUser.username).toBe('Princess Peach');
         expect(myUser.name).toBe('Monika');
         expect(myUser.bio).toBe("She's a queen, beach!");
         expect(myUser.dateCreated).toBe('dateCreated');
-        expect(myUser.lastUpdate).toBe('lastUpdate');
+        expect(myUser.lastUpdated).toBe('lastUpdated');
     });
 })
 
