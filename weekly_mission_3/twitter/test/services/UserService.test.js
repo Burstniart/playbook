4663f8d2869d25ad7bb7ebe1fcaa046951a7638e
Burstniart <v4gr33n@hotmail.com>
2022-04-20 16:03:03 -0500

    Add getAllUsernames method test

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -23,5 +23,14 @@ describe("Test for UserService", () => {
 
         expect(user.username).toBe('PrncssPeach')
     });
+    test("4. Given a list of users give me the list of usernames", () => {
+        const user1 = UserService.create(1, 'Princess1', 'Monika')
+        const user2 = UserService.create(1, 'Princess2', 'Monika')
+        const user3 = UserService.create(1, 'Princess3', 'Monika')
+        const usernames = UserService.getAllUsernames([user1, user2, user3])
+        expect(usernames).toContain('Princess1')
+        expect(usernames).toContain('Princess2')
+        expect(usernames).toContain('Princess3')
+    });
 })
 
