575dc8814f4a4d1889dd7d1e3b7209cb86076a92
Burstniart <v4gr33n@hotmail.com>
2022-04-19 22:58:30 -0500

    Check for not undefined date values

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,14 +1,15 @@
 const User = require('./../../app/models/User')
 describe('Unit Test for Twitter', () => {
     test('1) Model for User', () => {
-        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!", 'dateCreated', 'lastUpdated')
+        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!")
 
         expect(myUser.id).toBe(1);
         expect(myUser.username).toBe('Princess Peach');
         expect(myUser.name).toBe('Monika');
         expect(myUser.bio).toBe("She's a queen, beach!");
-        expect(myUser.dateCreated).toBe('dateCreated');
-        expect(myUser.lastUpdated).toBe('lastUpdated');
+        
+        expect(myUser.dateCreated).not.toBeUndefined();
+        expect(myUser.lastUpdated).not.toBeUndefined();
     });
 })
 
