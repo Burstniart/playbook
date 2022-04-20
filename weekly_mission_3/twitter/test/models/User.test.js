08af7e04fb0c666f501c9b759d7b572605ab719f
Burstniart <v4gr33n@hotmail.com>
2022-04-19 23:12:36 -0500

    Add test for getters

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,15 +1,22 @@
 const User = require('./../../app/models/User')
 describe('Unit Test for Twitter', () => {
     test('1) Model for User', () => {
-        const myUser = new User(1,'Princess Peach','Monika',"She's a queen, beach!")
+        const myUser = new User(1,'Princess_Peach','Monika',"She's a queen, beach!")
 
         expect(myUser.id).toBe(1);
-        expect(myUser.username).toBe('Princess Peach');
+        expect(myUser.username).toBe('Princess_Peach');
         expect(myUser.name).toBe('Monika');
         expect(myUser.bio).toBe("She's a queen, beach!");
-        
         expect(myUser.dateCreated).not.toBeUndefined();
         expect(myUser.lastUpdated).not.toBeUndefined();
+    });
+    test('2) Getters for User', () => {
+        const user = new User(2, 'Princess_Peach', 'Monika', "She's a queen, beach!")
+
+        expect(user.getUsername).toBe('Princess_Peach');
+        expect(user.getBio).toBe("She's a queen, beach!");
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeUndefined();
     });
 })
 
